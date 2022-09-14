9a7d50a667d838d3cfe69dceb0a31bafa2ec0ce5
gtconfirmed007 <gtconfirmed007@gmail.com>
2022-09-13 21:36:20 -0700

    task 0 and 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
